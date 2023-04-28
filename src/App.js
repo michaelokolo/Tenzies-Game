a44bf8b6f125ba2de0bc38542f263f482bc91dbf
michaelokolo <91018269+michaelokolo@users.noreply.github.com>
2023-04-28 10:55:41 +0100

    Styling tenzies game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import Die from './Die';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <main className="main">
+        <div className="die-container">
+          <Die value={1} />
+          <Die value={5} />
+          <Die value={3} />
+          <Die value={1} />
+          <Die value={4} />
+          <Die value={6} />
+          <Die value={2} />
+          <Die value={4} />
+          <Die value={6} />
+          <Die value={1} />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Die.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Die(props) {
+  return <p className="num">{props.value}</p>;
+}
+
+export default Die;
